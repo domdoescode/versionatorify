642889fb787534224af5abcc8eb6ff0ca3aa372b
Dom Udall <dom@synthmedia.co.uk>
2014-03-18 17:34:43 +0000

    Improving test layout and increasing amount of tests

--- test/transform.js
+++ test/transform.js
@@ -6,23 +6,42 @@ var fs = require('fs')
   , transform = require('..')(staticFileMap)
 
 describe('transform()', function () {
-  it('should do some transform thing', function (done) {
+  it('should return the mapped version path if it exists in the map', function (done) {
     var testFile = path.join(__dirname, 'fixtures', 'pre', 'basic.jade')
       , resultFile = path.join(__dirname, 'fixtures', 'post', 'basic.jade')
-      , data = ''
 
-    fs.createReadStream(testFile)
-      .pipe(transform(testFile))
+    testPrePost(testFile, resultFile, done)
+  })
+
+  it('should return the original path if it does not exist in the map', function (done) {
+    var testFile = path.join(__dirname, 'fixtures', 'pre', 'not-mapped.jade')
+      , resultFile = path.join(__dirname, 'fixtures', 'post', 'not-mapped.jade')
+
+    testPrePost(testFile, resultFile, done)
+  })
+
+  it('should do nothing if no versionPath is found', function (done) {
+    var testFile = path.join(__dirname, 'fixtures', 'pre', 'no-version-path.jade')
+      , resultFile = path.join(__dirname, 'fixtures', 'post', 'no-version-path.jade')
+
+    testPrePost(testFile, resultFile, done)
+  })
+
+  function testPrePost(pre, post, callback) {
+    var data = ''
+
+    fs.createReadStream(pre)
+      .pipe(transform(pre))
       .pipe(through(write, end))
 
     function write (buf) { data += buf }
     function end () {
-      fs.readFile(resultFile, 'utf-8', function (error, result) {
-        if (error) return done(error)
+      fs.readFile(post, 'utf-8', function (error, result) {
+        if (error) return callback(error)
 
         should.equal(data, result)
-        done()
+        callback()
       })
     }
-  })
+  }
 })
